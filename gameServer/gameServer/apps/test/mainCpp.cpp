a294bafc67a5046947e47a340b50ed6a87c52232
nick <hanxingchen1986@gmail.com>
2024-05-03 16:42:53 +0800

    111

---
+++ gameServer/gameServer/apps/test/mainCpp.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+int main(int argc, char** argv){
+
+	//NetBus::instance()->start_tcp_server(6080);
+	//NetBus::instance()->start_tcp_server(6081);
+
+	//NetBus::instance()->run();
+
+	getchar();
+	return 0;
+}
--- gameServer/gameServer/apps/test/main_test.c
+++ gameServer/gameServer/apps/test/main_test.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char** argv){
+
+#include "../../netbus/netbus.h"
+int main__11(int argc, char** argv){
 
 	printf("helloworld\n");
+
+	NetBus::instance()->start_tcp_server(6080);
+	NetBus::instance()->start_tcp_server(6081);
+
+	NetBus::instance()->run();
 
 	system("pause");
 	return 0;
