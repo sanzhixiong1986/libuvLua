1be09bdbeca1698614e83b8e6917315faca3368e
nick <hanxingchen1986@gmail.com>
2024-05-06 15:22:39 +0800

    给出websocket的方法

--- gameServer/gameServer/apps/test/mainCpp.cpp
+++ gameServer/gameServer/apps/test/mainCpp.cpp
@@ -9,9 +9,9 @@ using namespace std;
 #include "../../netbus/netbus.h"
 
 int main(int argc, char** argv){
-
+	netbus::instance()->init();
 	netbus::instance()->start_tcp_server(6080);
-	netbus::instance()->start_tcp_server(6081);
+	netbus::instance()->start_ws_server(8001);
 	netbus::instance()->run();
 
 	getchar();
