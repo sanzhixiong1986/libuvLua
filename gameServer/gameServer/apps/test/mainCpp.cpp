f8a18fa8ebeeb451e3c702600f2d9937e673dfc8
nick <hanxingchen1986@gmail.com>
2024-05-16 13:34:10 +0800

    1

--- gameServer/gameServer/apps/test/mainCpp.cpp
+++ gameServer/gameServer/apps/test/mainCpp.cpp
@@ -5,59 +5,49 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+
 #include "../../netbus/proto_man.h"
 #include "../../netbus/netbus.h"
-#include "../../utils/logger.h"
+#include "proto/pf_cmd_map.h"
 #include "../../utils/logger.h"
 #include "../../utils/time_list.h"
 #include "../../utils/timestamp.h"
-
-#include "proto/pf_cmd_map.h"
-
 #include "../../database/mysql_wrapper.h"
-
 #include "../../database/redis_wrapper.h"
-
-#include "../../lua_wrapper/lua_wrapper.h"
-
-
 
 static
 void on_logger_timer(void* udata) {
 	log_debug("on_logger_timer");
 }
 
-static void on_query_cb(const char* err, MYSQL_RES* result, void* udata) {
+static void
+on_query_cb(const char* err, MYSQL_RES* result, void* udata) {
 	if (err) {
 		printf("err");
 		return;
 	}
 
-	/*if (result) {  // 检查指针是否非空
-		for (const auto& row : *result) {  // 遍历每个内部向量
-			for (const auto& item : row) {  // 遍历内部向量中的每个字符串
-				std::cout << item << " ";
-			}
-			std::cout << std::endl;
-		}
-	}*/
-
 	printf("success");
-	std::cout << result;
 }
 
-static void on_open_cb(const char* err, void* context,void* udata) {
+static void
+on_open_cb(const char* err, void* context, void* udata) {
 	if (err != NULL) {
 		printf("%s\n", err);
 		return;
 	}
-	printf("connect success\n");
+	printf("connect success");
 
-	mysql_wrapper::query(context, "select * from note", on_query_cb);
+	// mysql_wrapper::query(context, "update class_test set name = \"blake haha\" where id = 8", on_query_cb);
+	mysql_wrapper::query(context, "select * from class_test", on_query_cb);
+
+	// mysql_wrapper::close(context);
 }
 
-static void test_db() {
-	mysql_wrapper::connect("127.0.0.1", 3306, "games", "root", "sanzhixiong", on_open_cb);
+static void
+test_db() {
+	mysql_wrapper::connect("127.0.0.1", 3306, "class_sql", "root", "123456", on_open_cb);
 }
 
 static void
@@ -66,6 +56,8 @@ on_redis_query(const char* err, redisReply* result, void* udata) {
 		printf("%s\n", err);
 		return;
 	}
+
+	printf("success\n");
 }
 
 static void
@@ -76,7 +68,7 @@ on_redis_open(const char* err, void* context, void* udata) {
 	}
 	printf("connect success\n");
 
-	redis_wrapper::query(context, "get hello", on_redis_query);
+	redis_wrapper::query(context, "select 1", on_redis_query);
 	// redis_wrapper::close_redis(context);
 }
 
@@ -85,43 +77,28 @@ test_redis() {
 	redis_wrapper::connect("127.0.0.1", 6379, on_redis_open);
 }
 
-/// <summary>
-/// 测试lua的代码
-/// </summary>
-static void text_lua() {
-	printf("启动lua的相关\n");
-	lua_wrapper::init();
-	lua_wrapper::exe_lua_file("./main.lua");
-	//lua_wrapper::exit();
-}
-
-int main(int argc, char** argv){
-
+int main(int argc, char** argv) {
 	proto_man::init(PROTO_BUF);
 	init_pf_cmd_map();
-	//时间戳的和log的相关操作
 	logger::init("logger/gateway/", "gateway", true);
-	/*
-	log_error("%d", timestamp());
-	log_error("%d", timestamp_today());
+
+	log_debug("%d", timestamp());
+	log_debug("%d", timestamp_today());
 	log_debug("%d", date2timestamp("%Y-%m-%d %H:%M:%S", "2018-02-01 00:00:00"));
+
 
 	unsigned long yesterday = timestamp_yesterday();
 	char out_buf[64];
 	timestamp2date(yesterday, "%Y-%m-%d %H:%M:%S", out_buf, sizeof(out_buf));
 	log_debug("%s", out_buf);
-	schedule(on_logger_timer, NULL, 3000, -1);
-	*/
+	// schedule(on_logger_timer, NULL, 3000, -1);
 
-	//test_db();
-	//test_redis();
-	
 	netbus::instance()->init();
 	netbus::instance()->tcp_listen(6080);
 	netbus::instance()->ws_listen(8001);
 	netbus::instance()->udp_listen(8002);
-	text_lua();
+
 	netbus::instance()->run();
-	lua_wrapper::exit();
+
 	return 0;
 }
