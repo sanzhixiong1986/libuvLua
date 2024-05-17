d56874573cb78e81b557ee1fe33aae26c695d80e
nick <hanxingchen1986@gmail.com>
2024-05-17 14:50:12 +0800

    added 添加talkroom

---
+++ unityClinet/moba_client/Assets/Scripts/talkroom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talkroom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void on_enter_talkroom()
+    {
+        Debug.Log("on_enter_talkroom");
+    }
+
+    public void on_exit_talkroom()
+    {
+        Debug.Log("on_exit_talkroom");
+    }
+
+    public void on_send_msg()
+    {
+        Debug.Log("on_send_msg");
+    }
+}
